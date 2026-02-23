67cba18eebb163f183a83eba3cd7c1985c933d84
sn2047-dot <sn2047@srmist.edu.in>
2026-02-23 09:50:34 +0530

    UCE hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,30 @@
 public class PalindromeCheckerApp {
 
+    static boolean palindrome(String word){
+
+        String rev_word = "";
+        for(int i = word.length() - 1; i >= 0; i--){
+            rev_word += word.charAt(i);
+        }
+
+        if (word.equals(rev_word)){
+            System.out.println("Palindrome");
+            return true;
+        }
+
+        else{
+            System.out.println("not");
+            return false;
+        }
+    }
+
     public static void main(String[] args){
 
         double version = 1.0;
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : " + version);
         System.out.println("System initialized successfully.");
+        palindrome("helo");
 
     }
 
