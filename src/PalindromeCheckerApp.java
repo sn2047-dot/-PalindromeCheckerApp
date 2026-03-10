74a4d88c298c20799b0b46769dc05dc0c7dd4f33
sn2047-dot <sn2047@srmist.edu.com>
2026-03-10 09:37:42 +0530

    UC13

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,70 +1,78 @@
 import java.util.*;
 
-interface PalindromeStrategy {
-    boolean checkPalindrome(String word);
-}
+public class UseCase13PalindromeCheckerApp {
 
-class StackStrategy implements PalindromeStrategy {
+    static boolean palindromeArray(String word){
+        int start = 0;
+        int end = word.length() - 1;
 
-    public boolean checkPalindrome(String word) {
+        while(start < end){
+            if(word.charAt(start) != word.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 
+    static boolean palindromeStack(String word){
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < word.length(); i++) {
+        for(int i = 0; i < word.length(); i++){
             stack.push(word.charAt(i));
         }
 
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != stack.pop()) {
+        for(int i = 0; i < word.length(); i++){
+            if(word.charAt(i) != stack.pop()){
                 return false;
             }
         }
 
         return true;
     }
-}
 
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean checkPalindrome(String word) {
-
+    static boolean palindromeDeque(String word){
         Deque<Character> deque = new ArrayDeque<>();
 
-        for (int i = 0; i < word.length(); i++) {
+        for(int i = 0; i < word.length(); i++){
             deque.addLast(word.charAt(i));
         }
 
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
+        while(deque.size() > 1){
+            if(deque.removeFirst() != deque.removeLast()){
                 return false;
             }
         }
 
         return true;
     }
-}
 
-public class PalindromeCheckerApp {
+    public static void main(String[] args){
 
-    public static void main(String[] args) {
-
-        System.out.println("Palindrome Checker App");
-
-        String word = "M a d a M";
-
+        String word = "m a d a M";
         word = word.toLowerCase();
         word = word.replace(" ", "");
 
-        PalindromeStrategy strategy;
+        long startTime, endTime;
 
-        strategy = new StackStrategy();
+        startTime = System.nanoTime();
+        boolean arrResult = palindromeArray(word);
+        endTime = System.nanoTime();
+        long arrayTime = endTime - startTime;
 
-        boolean result = strategy.checkPalindrome(word);
+        startTime = System.nanoTime();
+        boolean stackResult = palindromeStack(word);
+        endTime = System.nanoTime();
+        long stackTime = endTime - startTime;
 
-        if (result) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not Palindrome");
-        }
+        startTime = System.nanoTime();
+        boolean dequeResult = palindromeDeque(word);
+        endTime = System.nanoTime();
+        long dequeTime = endTime - startTime;
+
+        System.out.println("Array Method: " + arrResult + " | Time: " + arrayTime + " ns");
+        System.out.println("Stack Method: " + stackResult + " | Time: " + stackTime + " ns");
+        System.out.println("Deque Method: " + dequeResult + " | Time: " + dequeTime + " ns");
     }
 }
